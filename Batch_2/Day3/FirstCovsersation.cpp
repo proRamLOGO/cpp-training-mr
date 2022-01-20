51748aa499e85edd48eaf96ad2718b3e8eb11160
proRamLOGO <00shubhbansal@gmail.com>
2022-01-20 14:56:12 +0530

    3.2.1

---
+++ Batch_2/Day3/FirstCovsersation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // Output
+    // SYNTAX
+    // cout << ENTITY_1 << ENTITY_2 .... << ENTITY_N ;
+
+    cout << "     Hello     " << endl ;
+    cout << "     Hel" << "lo     " << endl ;
+
+    cout << "3+5" << endl;
+    cout << 3+5 << endl;
+    cout << 8 << endl;
+
+    cout << "3+5" << endl << 3+5 << endl << 8 << endl;
+
+    cout << endl << endl << endl;
+
+    cout << "Please enter your name: ";
+
+    // SYNTAX FOR DECLARATION OF VARIABLE
+    // DATA_TYPE VAR_NAME_1, VAR_NAME_2, ... VAR_NAME_N;
+
+    string name;
+
+    // INPUT SYNTAX
+    // cin >> VAR_NAME_1 >> VAR_NAME_2 .... >> VAR_NAME_N;
+
+    cin >> name ;
+
+    // Print "Hello ", name, "! How are you ?"
+    cout << "Hello " << name << "! How are you?" << endl;
+
+    cout << endl << endl << endl;
+
+}
