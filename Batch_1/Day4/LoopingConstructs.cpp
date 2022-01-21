e104ca9282c2245f8e0193a4f7ff3ef536ea548f
proRamLOGO <00shubhbansal@gmail.com>
2022-01-21 10:47:12 +0530

    d4.1.2

--- Batch_1/Day4/LoopingConstructs.cpp
+++ Batch_1/Day4/LoopingConstructs.cpp
@@ -3,8 +3,39 @@ using namespace std;
 
 int main() {
 
+    // LOOPRING CONSTRUCTS
 
-    
+    // 1. WHILE LOOP
+    // SYNTAX
 
+    // while ( CONDITION ) {
+    //     STATEMENTS
+    //     ...
+    // }
+
+    // FACTORIAL n!
+
+    cout << "Enter a number:  ";
+
+    int n;
+    cin >> n;
+
+    int fact, i;
+    fact = 1;
+    i = 1;
+
+    while ( i <= n ) {
+        
+        cout << "Changing fact from  " << fact;
+        
+        fact = fact * i;
+
+        cout << " to " << fact << endl;
+
+        i = i + 1;
+        
+    }
+
+    cout << n << "! = " << fact << endl;
 
 }
