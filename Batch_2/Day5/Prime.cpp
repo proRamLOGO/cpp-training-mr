4e0368e9afc9777745ab51d0170b8e0b7da887ee
proRamLOGO <00shubhbansal@gmail.com>
2022-01-24 15:22:25 +0530

    d3.2.1

--- Batch_2/Day5/Prime.cpp
+++ Batch_2/Day5/Prime.cpp
@@ -15,6 +15,8 @@ int main() {
     int n,d;
     cin >> n;
 
+
+    // Approach 1. n ops
     bool isPrime = true;
 
     for ( d=2 ;    d<n   &&   isPrime==true   ; d++ ) {
@@ -23,11 +25,41 @@ int main() {
             cout << d << " NOT PRIME" << endl;
         } 
     }
-// 1. d==n
-// 2. isprime false
-
     if ( isPrime==true ) {
-        cout << "NUM IS PRIME" << endl;
+        cout << "NUM IS PRIME" << endl << endl ;
     }
 
+
+
+    // Approach 2. n/2 ops
+    isPrime = true;
+
+    for ( d=2 ;    d<=n/2   &&   isPrime==true   ; d++ ) {
+        if ( n%d == 0 ) {
+            isPrime = false;
+            cout << d << " NOT PRIME" << endl;
+        } 
+    }
+    if ( isPrime==true ) {
+        cout << "NUM IS PRIME" << endl << endl ;
+    }
+
+    //               _
+    // Approach 3. _/n ops
+    isPrime = true;
+
+    for ( d=2 ;    d*d<=n   &&   isPrime==true   ; d++ ) {
+        if ( n%d == 0 ) {
+            isPrime = false;
+            cout << d << " NOT PRIME" << endl;
+        } 
+    }
+    if ( isPrime==true ) {
+        cout << "NUM IS PRIME" << endl << endl ;
+    }
+
+
+
+
+    cout << endl << endl ;
 }
