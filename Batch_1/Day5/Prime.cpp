f373bd6bb88cae936295ae10627abccbe27b0869
proRamLOGO <00shubhbansal@gmail.com>
2022-01-24 10:41:04 +0530

    d5.1.1

---
+++ Batch_1/Day5/Prime.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // 3. For Loop
+    // SYNTAX
+
+    // for ( INITIALIZATION ; CONDITION OF TERMINATION ; INCREMENT, DECREMENT ) {
+    //     STATEMENTS
+    //     ...
+    // }
+
+    int n,d,counter;
+    cin >> n;
+
+    // APPROACH 1 n ops -----------------------------
+    bool isPrime = true;
+
+    for ( d = 2, counter = 0 ;   ( (d < n) && isPrime )    ; d++, counter++ ) {
+        if ( n%d == 0  ) {
+            isPrime = false;
+            cout << "NOT PRIME    " << counter <<  endl;
+        } 
+    }
+
+    if ( isPrime==true ) {
+        cout << "PRIME      " << counter << endl;
+    }
+    cout << endl << endl;
+
+
+    // APPROACH 2   n/2 ops -----------------------------
+    isPrime = true;
+
+    for ( d = 2, counter = 0 ;   ( (d <= n/2) && isPrime )    ; d++, counter++ ) {
+        if ( n%d == 0  ) {
+            isPrime = false;
+            cout << "NOT PRIME    " << counter <<  endl;
+        } 
+    }
+
+    if ( isPrime==true ) {
+        cout << "PRIME      " << counter << endl;
+    }
+    cout << endl << endl;
+    
+    //                _
+    // APPROACH 3   _/n ops -----------------------------
+
+    isPrime = true;
+
+    for ( d = 2, counter = 0 ;   ( (d*d <= n) && isPrime )    ; d++, counter++ ) {
+        if ( n%d == 0  ) {
+            isPrime = false;
+            cout << "NOT PRIME      " << counter << endl;
+        } 
+    }
+
+    if ( isPrime==true ) {
+        cout << "PRIME      " << counter << endl;
+    }
+
+}
