c44467f09666efa9a5633a4f53e05fc851f63954
proRamLOGO <00shubhbansal@gmail.com>
2022-01-27 14:53:15 +0530

    d8.2.2

--- Batch_2/Day8/LinkedList.cpp
+++ Batch_2/Day8/LinkedList.cpp
@@ -13,6 +13,48 @@ struct node {
 
 int main() {
 
-    
+    node *head, *tail;
+    head = tail = nullptr;
+ 
+    int n;
+    cin >> n;
+
+    for ( int i = 0 ; i < n ; i++ ) {
+
+        int dataValue;
+        cin >> dataValue;
+
+        if ( i==0 ) {
+            // LL is empty
+            // head and tail are at null
+
+            head = new node;
+            (*head).next = nullptr;
+            (*head).data = dataValue;
+            tail = head;
+
+        } else {
+
+            // tail.next = (node*)malloc(sizeof(node)); //i+=a
+            (*tail).next = new node;                        //i++
+            tail = (*tail).next;
+            (*tail).data = dataValue;
+            tail->data = dataValue;
+            (*tail).next = nullptr;
+            tail->next = nullptr;
+        }
+        
+    }    
+
+    // LL has been created;
+
+    for ( node *itr = head ; itr != nullptr ; itr = itr->next ) {
+        cout << itr << "  :  " << itr->next << "  :  " << itr->data << '\n';
+    }
+    cout << endl;
+
 
 }
+
+
+// '\n' == endl;
