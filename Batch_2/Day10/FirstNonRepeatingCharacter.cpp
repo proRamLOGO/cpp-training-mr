153ea2b41e73aabf692b3b139274067f1ccc49ba
proRamLOGO <00shubhbansal@gmail.com>
2022-01-31 14:27:05 +0530

    d10.2.3

---
+++ Batch_2/Day10/FirstNonRepeatingCharacter.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main() {
+
+    string s;
+    cin >> s;
+
+    int freq[26];
+    queue<char> q;
+
+    for ( char ch:s ) {
+
+        freq[ch-'a']++;
+
+        while( !q.empty() && freq[q.front()-'a']>1 )
+            q.pop();
+        
+        if ( freq[ch-'a']==1 ) 
+            q.push(ch);
+
+        if ( q.empty() )
+            cout << '#';
+        else 
+            cout << q.front();
+
+    }
+
+    cout << endl;
+
+}
+
+
+ 
